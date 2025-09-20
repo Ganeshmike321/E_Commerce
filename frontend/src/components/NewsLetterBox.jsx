75612b4aed215ffe90ed82c0363b87b0ef2bbe5b
Ganeshmike321 <ganeshkumarbass@gmail.com>
2025-09-20 17:45:52 +0530

    Add NewsLetterBox component

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function NewsLetterBox() {
+    function onSubmitHandler(event) {
+        event.preventDefault()
+
+    }
+
+    return (
+        <div className='text-center'>
+            <p>Subscribe now & get 20% off </p>
+            <p>Exclusive offers and new arrivals straight to your inbox.</p>
+            <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3' >
+                <input className='w-full sm:flex-1 outline-none' required type='email' placeholder='Enter your email' />
+                <button className='bg-black text-white text-xs px-10 py-4' type='submit'>SUBSCRIBE</button>
+            </form>
+        </div>
+    )
+}
+
